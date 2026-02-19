1646f26fc08ab5f7574825379699c881a1bb8e67
Galhoom <102315329+Galhoom22@users.noreply.github.com>
2026-02-19 23:42:58 +0200

    87. Theme to views 3

---
+++ resources/views/theme/partials/scripts.blade.php
@@ -0,0 +1,6 @@
+<script src="vendors/jquery/jquery-3.2.1.min.js"></script>
+<script src="vendors/bootstrap/bootstrap.bundle.min.js"></script>
+<script src="vendors/owl-carousel/owl.carousel.min.js"></script>
+<script src="{{asset("assets")}}/js/jquery.ajaxchimp.min.js"></script>
+<script src="{{asset("assets")}}/js/mail-script.js"></script>
+<script src="{{asset("assets")}}/js/main.js"></script>
